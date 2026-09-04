b32d805dc6081e8ad7dd070d9d57eb74cf8b8c1b
Konstantin <orzhekhovsky@gmail.com>
2026-09-04 23:15:16 +0300

    Update main.py

--- Virtual Study Assistant/main.py
+++ Virtual Study Assistant/main.py
@@ -1,13 +1,13 @@
-# Write your solution here
-subject = input()
+subjects = {"Math": 90, "Science": 120}
+
 total_study_time = 0
-total_time = 0
 
-for key, value in subjects.items():
-    print(f"{key}: {value} minutes")
-    total_study_time += value
+for subject, minutes in subjects.items():
+    print(f"{subject}: {minutes} minutes")
+    total_study_time += minutes
 
-total_time = (total_study_time//45)*15 + total_study_time
+breaks = (total_study_time - 1) // 45
+total_time = total_study_time + breaks * 15
 
 print(f"Total time: {total_study_time} minutes")
 print(f"Total time including breaks: {total_time} minutes")
